56d87aafbe32a00729535badce38322bb45c3ea6
Israel Jardim <israel@aucon.com.br>
2019-09-09 11:43:50 -0300

    Form new user

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = (app) => {
-    app.use('/', require('./controllers/main/index'))
+    app.use('/', require('./controllers/main/index'));
+    app.use('/users', require('./controllers/users/index'));
 }
